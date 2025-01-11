6e6486191715262facc8e78188d2622c0ebdf292
Agnieszka Roguska <agnieszka.roguska@yahoo.com>
2025-01-11 17:21:32 +0100

    project implementation v2

--- src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-//@RestController
 public class HelloController {
     @GetMapping(value = "/")
+    @ResponseBody
     public String hello() {
         return "Hello Vistula, it is my first Spring Controller!";
     }
@@ -19,5 +19,4 @@ public class HelloController {
         model.addAttribute("name", name);
         return "greeting";
     }
-    // http://localhost:8080/greeting?name=Vistula
 }
